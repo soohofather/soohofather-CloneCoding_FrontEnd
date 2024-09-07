cca430a964255ac3ca87521684c18e5e091e2b21
soohofather <enthchal@gmail.com>
2024-09-07 10:36:37 +0900

    도전

---
+++ mall/src/pages/todo/ListPage.js
@@ -0,0 +1,17 @@
+import ListComponent from "../../components/todo/ListComponent";
+
+const ListPage = () => {
+
+  return ( 
+  <div className="p-4 w-full bg-white">
+    <div className="text-3xl font-extrabold">
+      Todo List Page Component 
+    </div> 
+
+    <ListComponent/>
+
+  </div>
+   );
+}
+ 
+export default ListPage;
