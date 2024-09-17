fe2e2617413b7c45bf97a4b4d5ff1c7ec455ded8
soohofather <enthchal@gmail.com>
2024-09-17 16:05:31 +0900

    섹션 6 : 상품 조회 컴포넌트 처리

--- mall/src/router/productsRouter.js
+++ mall/src/router/productsRouter.js
@@ -6,6 +6,8 @@ const ProductsList =  lazy(() => import("../pages/products/ListPage"))
 
 const ProductsAdd = lazy(() => import("../pages/products/AddPage"))
 
+const ProductsRead = lazy(() => import("../pages/products/ReadPage"))
+
 const ProductsRouter = () => {
     return [
         {
@@ -20,6 +22,10 @@ const ProductsRouter = () => {
             path: "add",
             element: <Suspense fallback={Loading}><ProductsAdd/></Suspense>
         },
+        {
+            path: "read/:pno",
+            element: <Suspense fallback={Loading}><ProductsRead/></Suspense>
+        }
 
     ]
 }
