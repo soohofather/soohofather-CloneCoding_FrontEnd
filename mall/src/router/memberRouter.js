eadc3831fd3e84a2a803e2446577dbb2b521aad9
soohofather <enthchal@gmail.com>
2024-10-25 14:13:25 +0900

    수정 페이지 추가

--- mall/src/router/memberRouter.js
+++ mall/src/router/memberRouter.js
@@ -3,6 +3,7 @@ const Loading = <div>Loading....</div>
 const Login =  lazy(() => import("../pages/member/LoginPage"))
 const Logout = lazy(() => import("../pages/member/LogoutPage"))
 const KakaoRedirect = lazy(() => import("../pages/member/KakaoRedirectPage"))
+const MemberModify = lazy(() => import("../pages/member/ModifyPage"))
 
 
 const memberRouter = () => {
@@ -21,7 +22,11 @@ const memberRouter = () => {
     {
       path:"kakao",
       element: <Suspense fallback={Loading}><KakaoRedirect/></Suspense>
-    }
+    },
+    {
+      path:"modify",
+      element: <Suspense fallback={Loading}><MemberModify/></Suspense>,
+    },
   ]
 }
 
